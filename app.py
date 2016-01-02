e7dbbaac1f56dc243dd0991cc0b21cb2c7a78be6
Jin Park <jin+github@jinpark.net>
2016-01-02 14:35:08 +0900

    add flask-migrations

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from sqlalchemy_searchable import SearchQueryMixin
 from sqlalchemy_utils.types import TSVectorType
 from sqlalchemy_searchable import make_searchable
 
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jinpark:password1@localhost:5432/flask_fts_local'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 make_searchable()
 
@@ -14,11 +21,13 @@ class DocumentQuery(BaseQuery, SearchQueryMixin):
     pass
 
 class Document(db.Model):
+    __table_name__ = 'documenttest'
     query_class = DocumentQuery
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=True)
     description = db.Column(db.Text)
+    cat = db.Column(db.Text)
+
     search_vector = db.Column(TSVectorType('title', 'description'))
 
     def __init__(self, title, description):
@@ -41,4 +50,5 @@ def search():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    # app.run()
+    manager.run()
