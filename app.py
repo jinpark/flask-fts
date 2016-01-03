e3bf9c617785d3a345c47db7cbf68bcbf3f1fe0c
Jin Park <jin+github@jinpark.net>
2016-01-03 23:22:09 +0900

    add place and postgis/geoalchemy search

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy, BaseQuery
-from sqlalchemy_searchable import SearchQueryMixin
+from sqlalchemy import func
 from sqlalchemy_utils.types import TSVectorType
-from sqlalchemy_searchable import make_searchable
+from sqlalchemy_searchable import make_searchable, SearchQueryMixin, search
+from geoalchemy2 import Geometry, Geography, WKTElement
 
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -21,13 +22,10 @@ class DocumentQuery(BaseQuery, SearchQueryMixin):
     pass
 
 class Document(db.Model):
-    __table_name__ = 'documenttest'
     query_class = DocumentQuery
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), unique=True)
     description = db.Column(db.Text)
-    cat = db.Column(db.Text)
-
     search_vector = db.Column(TSVectorType('title', 'description'))
 
     def __init__(self, title, description):
@@ -37,15 +35,51 @@ class Document(db.Model):
     def __repr__(self):
         return '<Document %r>' % self.title
 
+class PlaceQuery(BaseQuery, SearchQueryMixin):
+    pass
+
+class Place(db.Model):
+    query_class = PlaceQuery
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), unique=True)
+    description = db.Column(db.Text)
+    search_vector = db.Column(TSVectorType('title', 'description'))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    point = db.Column(Geography(geometry_type='POINT', srid=4326))
+
+    def __init__(self, title, description, latitude, longitude):
+        self.title = title
+        self.description = description
+        self.latitude = latitude
+        self.longitude = longitude
+        self.point = "SRID=4326;POINT({} {})".format(latitude, longitude)
+
+    def __repr__(self):
+        return '<Place %r>' % self.title
+
 
 @app.route('/search', methods=['GET', 'POST'])
-def search():
+def search_text():
     if request.method == 'POST':
         search_term = request.form['search_term']
         results = Document.query.search(search_term)
         return render_template('search_results.html', results=results, search_term=search_term)
     else:
         return render_template('search.html')
+
+@app.route('/search_place', methods=['GET', 'POST'])
+def search_place():
+    if request.method == 'POST':
+        search_term = request.form['search_term']
+        distance = request.form['distance']
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        point = WKTElement('POINT({0} {1})'.format(latitude, longitude), srid=4326)
+        results = search(db.session.query(Place).filter(func.ST_DWithin(Place.point, point, float(distance))), search_term)
+        return render_template('search_place_results.html', results=results, search_term=search_term, distance=distance, latitude=latitude, longitude=longitude)
+    else:
+        return render_template('search_place.html')
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -25,4 +25,15 @@ db.session.add(a)
 db.session.add(b)
 db.session.add(c)
 db.session.commit()
+
+d = Place('dcamp', 'study coworking', 37.50789, 127.04529)
+e = Place('maru180', 'party space', 37.495419, 127.038849)
+f = Place('brians coffee', 'cafe cool', 37.48391, 127.046063)
+
+db.session.add(d)
+db.session.add(e)
+db.session.add(f)
+db.session.commit()
+
+
 ```
